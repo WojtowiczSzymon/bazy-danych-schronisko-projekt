bce80a61f511bb72258711beae8ff630b1d61a73
mista03 <michal.stachowiak@student.put.poznan.pl>
2026-01-07 14:36:13 +0100

    początkowy commit

---
+++ f120/readable/application/supporting_objects/install_scripts/dml.sql
@@ -0,0 +1,40 @@
+INSERT INTO schroniska(nazwa, adres, telefon, email)
+VALUES ('Schronisko Poznań', 'Poznań, ul. Zwierzęca 12', '123456789', 'kontakt@poznan.pl');
+
+INSERT INTO pomieszczenia(typ, budynek, ilosc_kojcy, id_schroniska)
+VALUES ('Kociarnia', 'A', 30, 1);
+INSERT INTO pomieszczenia(typ, budynek, ilosc_kojcy, id_schroniska)
+VALUES ('Psiarnia', 'A', 20, 1);
+INSERT INTO pomieszczenia(typ, budynek, ilosc_kojcy, id_schroniska)
+VALUES ('Magazyn', 'B', 0, 1);
+
+INSERT INTO opinie(tresc, data, id_schroniska)
+VALUES ('Bardzo miła obsługa i zadbane zwierzęta.', SYSDATE-10, 1);
+INSERT INTO opinie(tresc, data, id_schroniska)
+VALUES ('Adopcja przebiegła sprawnie.', SYSDATE-3, 1);
+
+INSERT INTO pracownicy(pesel, imie, nazwisko, etat, wynagrodzenie, data_ur, data_zatr, telefon, id_schroniska)
+VALUES ('90010112345', 'Anna', 'Kowalska', 'opiekun', 5000, DATE '1990-01-01', SYSDATE-200, '555111222', 1);
+INSERT INTO pracownicy(pesel, imie, nazwisko, etat, wynagrodzenie, data_ur, data_zatr, telefon, id_schroniska)
+VALUES ('85050567890', 'Marta', 'Nowak', 'dyrektor', 7200, DATE '1985-05-12', SYSDATE-500, '333444555', 1);
+
+INSERT INTO wolontariusze(pesel, imie, nazwisko, data_ur, telefon, id_schroniska)
+VALUES ('02020212345', 'Marta', 'Lis', DATE '2002-02-02', '666777888', 1);
+INSERT INTO wolontariusze(pesel, imie, nazwisko, data_ur, telefon, id_schroniska)
+VALUES ('03030354321', 'Piotr', 'Wilk', DATE '2003-03-03', '666111222', 1);
+
+INSERT INTO zasoby(typ, nazwa, ilosc, id_schroniska)
+VALUES ('Karma', 'Karma dla psów', 120, 1);
+INSERT INTO zasoby(typ, nazwa, ilosc, id_schroniska)
+VALUES ('Środki czystości', 'Płyn dezynfekujący', 40, 1);
+INSERT INTO zasoby(typ, nazwa, ilosc, id_schroniska)
+VALUES ('Karma', 'Karma dla kotów', 80, 1);
+
+INSERT INTO zwierzeta(imie, gatunek, data_ur, id_pomieszczenia)
+VALUES ('Klops', 'Pies', DATE '2019-08-15', 10);
+INSERT INTO zwierzeta(imie, gatunek, data_ur, id_pomieszczenia)
+VALUES ('Simba', 'Pies', DATE '2023-07-11', 10);
+INSERT INTO zwierzeta(imie, gatunek, data_ur, id_pomieszczenia)
+VALUES ('Stefan', 'Kot', DATE '2022-09-13', 20);
+INSERT INTO zwierzeta(imie, gatunek, data_ur, id_pomieszczenia)
+VALUES ('Puszek', 'Kot', DATE '2018-01-30', 20);
